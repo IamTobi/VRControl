11a90435fb20e2f667eaf3af86d98eb230e6c064
Tobi <toble@outlook.com>
2015-01-28 17:44:44 +0100

    doc+unity

---
+++ unity-project/Assets/Scripts/Cardboard/CharacterController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterController : MonoBehaviour {
+	
+	private Vector3 startPosition,endPosition;
+
+	// Use this for initialization
+	void Start () {
+		startPosition = new Vector3(0,0,0);
+		endPosition = new Vector3(0,0,15);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Cardboard.SDK.CardboardTriggered){
+			this.transform.Translate(Vector3.forward *Time.deltaTime);
+		}
+	
+	}
+
+	void MoveCamera(Transform thisTransform, float time,Vector3 startPos, Vector3 endPos){
+		float i = 0f;
+		float rate = 1f/time;
+		thisTransform.position = Vector3.Lerp (startPos,endPos,i);
+	}
+}
