ffd2ec1dc09589296447f31c02b5a04f41c7b37b
Tobi <toble@outlook.com>
2015-02-04 17:00:44 +0100

    cb and dd project files

---
+++ unity-project-cb/Assets/Scripts/Autowalk.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Autowalk : MonoBehaviour {
+
+	private CardboardHead head;
+	private Vector3 startPosition;
+
+	// Use this for initialization
+	void Start () {
+		head = Camera.main.GetComponent<StereoController>().Head;
+		startPosition = transform.localPosition;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(Cardboard.SDK.CardboardTriggered){
+			GameObject FPSController = GameObject.Find ("Head");
+			FPSInputController autowalk = FPSController.GetComponent<FPSInputController>();
+			autowalk.checkAutoWalk = !autowalk.checkAutoWalk;
+		}
+	}
+}
