fd2cb15246c4a7e4ba62a91a055a4021980af340
Tobi <toble@outlook.com>
2015-02-04 15:23:04 +0100

    unity+doc

---
+++ unity-project/Assets/Scripts/Cardboard/CharacterControl.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterControl : MonoBehaviour {
+
+	private bool triggered;
+
+	// Use this for initialization
+	void Start () {
+
+		triggered=false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Cardboard.SDK.CardboardTriggered) {
+			while(triggered=false){
+				transform.Translate(Vector3.forward*Time.deltaTime);
+			}
+
+		}
+	}
+}
